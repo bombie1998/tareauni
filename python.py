aa72f707b379a0a9ae469863b79d194a9ee9c829
bombie1998 <rafaelln0498@gmail.com>
2023-07-21 01:28:57 -0500

    primer repositorio

---
+++ python.py
@@ -0,0 +1,7 @@
+verbo = input ("Ingrese el verbo: ")
+adjetivo = input("Ingrese el adjetivo: ")
+sustantivo_plural = input ("Ingrese el sustantivo plural: ")
+
+historia = f"Me apatece demasiado {verbo}. Por lo que haria lo que fuese por {verbo}. Entonces me pregunto si el dia de mañana Jose, mi primo, podria acompañarme un momento al cine para {adjetivo} y luego poder conseguir nuestras {sustantivo_plural}."
+
+print(historia)
